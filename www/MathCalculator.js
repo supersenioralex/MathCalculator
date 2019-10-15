08c033f11340efb0a0db6f56a9d37fb8ad027271
dev-donR <mountelbrus27@outlook.com>
2019-10-15 01:12:52 +0100

    Initial commit

---
+++ www/MathCalculator.js
@@ -0,0 +1,12 @@
+var exec = require('cordova/exec');
+
+
+module.exports.add = function(arg0, success, error) 
+{
+    exec(success, error, 'MathCalculator', 'add', [arg0])
+}
+
+module.exports.substract = function(arg0, success, error) 
+{
+    exec(success, error, 'MathCalculator', 'substract', [arg0])
+}
